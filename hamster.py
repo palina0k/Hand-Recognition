3959df2d5f65b50b6ea7099b79a34bbfb0f03d16
Palina Karzhenka <korzik.polina@gmail.com>
2025-12-05 19:10:52 -0800

    Fix left hand detection, add another reaction image

--- hamster.py
+++ hamster.py
@@ -1,12 +1,14 @@
 import cv2
 import mediapipe as mp
+import math
 
 # Loading images
 normal = cv2.imread("hamster_normal.jpg")
 peace = cv2.imread("hamster_peace_sign.jpg")
 scared = cv2.imread("hamster_scared.jpg")
+thinking = cv2.imread("thinking.jpg")
 
-if normal is None or peace is None or scared is None:
+if normal is None or peace is None or scared is None or thinking is None:
     print("Error: Could not load the images")
     exit()
 
@@ -20,7 +22,8 @@ mp_drawing_styles = mp.solutions.drawing_styles
 face_mesh = mp_face_mesh.FaceMesh(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5,
-    max_num_faces=1
+    max_num_faces=1,
+    refine_landmarks=True
 )
 
 hands = mp_hands.Hands(
@@ -30,6 +33,27 @@ hands = mp_hands.Hands(
 )
 
 cap = cv2.VideoCapture(0)
+
+def calculate_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def get_mouth_center(face_landmarks, width, height):
+    upper_lip_points = [13, 14, 80, 81]
+    lower_lip_points = [17, 84, 314, 178]
+    
+    mouth_points = []
+    for idx in upper_lip_points + lower_lip_points:
+        if idx < len(face_landmarks.landmark):
+            landmark = face_landmarks.landmark[idx]
+            mouth_points.append((int(landmark.x * width), int(landmark.y * height)))
+    
+    if mouth_points:
+        center_x = sum([p[0] for p in mouth_points]) // len(mouth_points)
+        center_y = sum([p[1] for p in mouth_points]) // len(mouth_points)
+        return (center_x, center_y)
+    
+    return (int(face_landmarks.landmark[13].x * width), 
+            int(face_landmarks.landmark[13].y * height))
 
 while True:
     ret, frame = cap.read()
@@ -43,9 +67,13 @@ while True:
 
     face_results = face_mesh.process(rgb_frame)
     mouth_open = False
+    mouth_center = None
+    finger_touching_mouth = False
 
     if face_results.multi_face_landmarks:
         for face_landmarks in face_results.multi_face_landmarks:
+            mouth_center = get_mouth_center(face_landmarks, width, height)
+            
             upper_lip = face_landmarks.landmark[13]
             lower_lip = face_landmarks.landmark[14]
 
@@ -57,28 +85,55 @@ while True:
                 image=frame,
                 landmark_list=face_landmarks,
                 connections=mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
+                landmark_drawing_spec=None,
+                connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
             )
 
     hand_results = hands.process(rgb_frame)
     finger_count = 0
 
     if hand_results.multi_hand_landmarks:
-        for hand_landmarks in hand_results.multi_hand_landmarks:
+
+        for hand_idx, hand_landmarks in enumerate(hand_results.multi_hand_landmarks):
             lm = hand_landmarks.landmark
 
+            handedness = "right"
+            if hand_results.multi_handedness:
+                handedness = hand_results.multi_handedness[hand_idx].classification[0].label.lower()
+            
             tips = [8, 12, 16, 20]
             pips = [6, 10, 14, 18]
 
             thumb_tip = lm[4]
-            thumb_ip = lm[3]
+            wrist = lm[0]
+            index_mcp = lm[5]
 
-            if thumb_tip.x < thumb_ip.x:
-                finger_count += 1
+            if handedness == "right":
+                if thumb_tip.x < index_mcp.x:
+                    finger_count += 1
+            else:
+                if thumb_tip.x > index_mcp.x:
+                    finger_count += 1
 
             for tip_idx, pip_idx in zip(tips, pips):
                 if lm[tip_idx].y < lm[pip_idx].y:
                     finger_count += 1
+
+            finger_tips = [
+                (int(lm[8].x * width), int(lm[8].y * height)),   # Index
+                (int(lm[12].x * width), int(lm[12].y * height)),  # Middle
+                (int(lm[16].x * width), int(lm[16].y * height)),  # Ring
+                (int(lm[20].x * width), int(lm[20].y * height)),  # Pinky
+                (int(lm[4].x * width), int(lm[4].y * height))     # Thumb
+            ]
+            
+            touch_threshold = 50
+            for finger_tip in finger_tips:
+                distance = calculate_distance(finger_tip, mouth_center)
+                if distance < touch_threshold:
+                    finger_touching_mouth = True
+                    break 
+            
 
             mp_drawing.draw_landmarks(
                 frame,
@@ -88,7 +143,9 @@ while True:
                 mp_drawing_styles.get_default_hand_connections_style()
             )
 
-    if finger_count == 2 and not mouth_open:
+    if finger_touching_mouth:
+        current_img = thinking.copy()
+    elif finger_count == 2 and not mouth_open:
         current_img = peace.copy()
     elif mouth_open:
         current_img = scared.copy()
